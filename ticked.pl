52fe7170c9c63ef1c885ab3ba530a55fefebae32
Deji Adegbite <d2alphame@yahoo.com>
2020-07-30 20:20:28 +0100

    WIP: Headings should be surrounded by blank lines

--- ticked.pl
+++ ticked.pl
@@ -1,24 +1,30 @@
 use v5.26;
 
 my $tmplines;                           # For temporarily holding a line
-my @tokens;                             # For use as a stack for 
+my $prev = "";                          # Holds the content of a previous line
 
 
 # Read files specified in stdin
 while(<>) {
 
+    # Headings should be surrounding by blank lines
+
     # H1 begins with 4 hashes (#) and at least 1 tab and must contain at least
     # 1 non-whitespace character. They must also be followed by a blank line
-    if(/^####\t\s*[^\s].*/) {
+    if(($prev eq ""  || $prev =~ /^\s*$/ ) && /^####\t\s*[^\s].*/) {
 
         # If we had something that looked like an H1, then read the next line
         # to ensure it's blank. If it is, then the previous line was an H1
         $tmplines = $_;
+
+        $prev = $_;                             # Save the current content of the line before loading the next one
         $_ = <>;                 
 
         if(/^\s*$/) {
             $tmplines =~ s%^####\t\s*([^\s].*)%<h1>$1</h1>%;
             print $tmplines;
+
+            $prev = $_;                         #  Again, save the content of the current line before loading the next one
             next;
         }
 
@@ -30,16 +36,20 @@ while(<>) {
 
     # H2 begins with 4 equals (=) and at least 1 tab and must contain at least
     # 1 non-whitespace character. They must also be followed by a blank line
-    if(/^====\t\s*[^\s].*/) {
+    if(($prev eq ""  || $prev =~ /^\s*$/ ) && /^====\t\s*[^\s].*/) {
 
         # If we had something that looked like an H2, then read the next line
         # to ensure it's blank. If it is, then the previous line was an H2
         $tmplines = $_;
+
+        $prev = $_;                             # Save the current content of the line before loading the next one
         $_ = <>;                 
 
         if(/^\s*$/) {
             $tmplines =~ s%^====\t\s*([^\s].*)%<h2>$1</h2>%;
             print $tmplines;
+
+            $prev = $_;                         #  Again, save the content of the current line before loading the next one
             next;
         }
 
